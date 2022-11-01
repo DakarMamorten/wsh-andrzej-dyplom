e8bb364305c7be598b4a1713327800a59ff0fd32
Vladyslav Paun <paunvlad122@gmail.com>
2022-11-01 16:26:40 +0100

    Add enum role

--- src/main/java/com/dyplom/domain/Users.java
+++ src/main/java/com/dyplom/domain/Users.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,7 +16,8 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name="role")
+    private Role role;
     private String username;
     private String password;
 }
