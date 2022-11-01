28431aabf200b3dac41745b5ba0a2d14b18e45ac
Vladyslav Paun <paunvlad122@gmail.com>
2022-11-01 15:58:00 +0100

    Create project

---
+++ src/main/java/com/dyplom/WshAndrzejDyplomApplication.java
@@ -0,0 +1,13 @@
+package com.dyplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshAndrzejDyplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WshAndrzejDyplomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyplom/wsh/WshAndrzejDyplomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyplom.wsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshAndrzejDyplomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
